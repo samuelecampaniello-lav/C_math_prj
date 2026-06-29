afb227560a3ad8fc1f978f6b32a1f6485ca0e697
Samuele Campaniello <samuelecampaniello.lav@gmail.com>
2026-06-29 21:20:56 +0200

    Little things

--- main/main1.c
+++ main/main1.c
@@ -16,24 +16,8 @@
 
 int main () 
 {
-    // Declare variables    
-    matrix* A=matrix_init(3,3);
-
-    // Fill matrix
-    A->data[0]=178; A->data[3]=4750; A->data[6]=23050;
-    A->data[1]=210; A->data[4]=92; A->data[7]=94252;
-    A->data[2]=1290; A->data[5]=169; A->data[8]=10;
-
-    // Transpose
-    matrix* B = matrix_transp1(A);
     
-    // Print det and tr of matrix
-    printf("det[A]= %f \n", matrix_det(B));
-    printf("Tr[A] = %f \n", matrix_tr(B));
-
-    // Free memory
-    matrix_free(A); matrix_free(B);
-
+    printf("Welcome to the program!\n");
     exit(0);
 
 }
